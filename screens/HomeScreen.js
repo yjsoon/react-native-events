5534db46a344fb85878057dcd2beeff7cafdc5d8
YJ Soon <yjsoon@tinkertanker.com>
2020-12-04 11:45:20 +0800

    Added a card for Home

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,11 +1,41 @@
 import React from "react";
-import { Text, View, StyleSheet } from "react-native";
+import { View, StyleSheet, Image } from "react-native";
 import { createStackNavigator } from "@react-navigation/stack";
+import {
+  Text,
+  Card,
+  CardItem,
+  Left,
+  Thumbnail,
+  Body,
+  Button,
+  Icon,
+} from "native-base";
 
 function HomeMainScreen() {
   return (
-    <View style={{ flex: 1, alignItems: "center" }}>
+    <View style={{ padding: 10 }}>
       <Text style={styles.titleText}>International Potato Day</Text>
+      <Card style={styles.card}>
+        <CardItem>
+          <Body>
+            <Text style={styles.cardTitle}>Welcome</Text>
+            <Text>
+              Thanks for downloading our app! Here, you'll find all kinds of
+              information about our upcoming event. It'll be great!
+            </Text>
+          </Body>
+        </CardItem>
+        <CardItem cardBody>
+          <Image
+            source={{
+              uri:
+                "https://upload.wikimedia.org/wikipedia/commons/a/ab/Patates.jpg",
+            }}
+            style={{ height: 200, width: null, flex: 1 }}
+          />
+        </CardItem>
+      </Card>
     </View>
   );
 }
@@ -26,5 +56,11 @@ const styles = StyleSheet.create({
     fontWeight: "bold",
     textAlign: "center",
     marginTop: 20,
+    marginBottom: 20,
+  },
+  cardTitle: {
+    fontSize: 20,
+    fontWeight: "bold",
+    marginBottom: 10,
   },
 });
